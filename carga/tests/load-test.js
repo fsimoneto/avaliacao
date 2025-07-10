a03ec0d6a504ae00494c6080b8ae66dbde3de7b4
Felipe <fnsimoneto@gmail.com>
2025-07-10 17:32:17 -0300

    adicionado if always

--- carga/tests/load-test.js
+++ carga/tests/load-test.js
@@ -3,8 +3,8 @@ const { sleep, check } = require('k6');
 const { htmlReport } = require('https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js');
 
 exports.options = {
-  vus: 500,
-  duration: '5m',
+  vus: 10,
+  duration: '10s',
   thresholds: {
     'http_req_duration': ['p(95)<2000'],  // 95% das requisições devem responder em menos de 2000ms
     'http_req_failed': ['rate<0.01'],     // Menos de 1% das requisições podem falhar
